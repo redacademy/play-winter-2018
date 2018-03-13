f708cfc902c3da2999766cdb9cd1f9e17035072c
Cliff Chan <cchan17@live.ca>
2018-03-13 14:59:13 -0700

    footer for mobile done

--- themes/integrate-play/footer.php
+++ themes/integrate-play/footer.php
@@ -14,10 +14,17 @@
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav>
 
+				<div class="social-media">
+					<i class="fa fa-linkedin"></i>
+					<i class="fa fa-twitter"></i>
+					<i class="fa fa-instagram"></i>
+					<i class="fa fa-facebook-f"></i>
+				</div>
+
 				<div class="site-info">
-					<h2>Integrate Play Solutions</h2>
-					<h3>Vancouver, BC, Canada</h3>
-					<h3>604-773-2546</h3>
+					<h2 class="footer__company">Integrate Play Solutions</h2>
+					<h3 class="footer__contact">Vancouver, BC, Canada</h3>
+					<h3 class="footer__number">604-773-2546</h3>
 				</div><!-- .site-info -->	
 
 				</footer><!-- #colophon -->
