0bd459e50922cc67cdfd302dc1581eeef829f496
Eric Chan <ericchan277@gmail.com>
2018-03-14 08:37:51 -0700

    Fixed issue where closing hamburger menu in mobile would disable the desktop navigation menu as well

--- themes/integrate-play/js/integrate-scripts.js
+++ themes/integrate-play/js/integrate-scripts.js
@@ -12,20 +12,20 @@
 
   /** Maybe better in media query */
 
-  // $(window).resize(function () { // grabbed from http://jsfiddle.net/UqYCr/
-    // if (document.body.clientWidth < 600) {
+  $(window).resize(function () { // grabbed from http://jsfiddle.net/UqYCr/
+    if (document.body.clientWidth < 600) {
       // $('.header-logo').removeClass('desktop-logo');
       // $('.header-logo').addClass('mobile-logo');
       // $('.menu-menu-1-container').hide()
-      // $('.menu-menu-1-container').css('display', 'none')
+      $('.menu-menu-1-container').css('display', 'none')
 
-    // } else {
+    } else {
       // $('.header-logo').removeClass('mobile-logo');
       // $('.header-logo').addClass('desktop-logo');
       // $('.menu-menu-1-container').show();
-      // $('.menu-menu-1-container').css('display', 'flex')
-    // }
-  // })
+      $('.menu-menu-1-container').css('display', 'flex')
+    }
+  })
 
 
   $('#menu-toggle').on('click', function () {
