690b926f2f7e76c9bad0e6b330156a25b4632671
Eric Chan <ericchan277@gmail.com>
2018-03-15 11:58:46 -0700

    Fixed contact image, added icons to mobile nav menu

--- themes/integrate-play/js/integrate-scripts.js
+++ themes/integrate-play/js/integrate-scripts.js
@@ -2,39 +2,25 @@
 
 (function ($) {
 
-  /** Maybe better in media query */
-
-  $(window).resize(function () { // grabbed from http://jsfiddle.net/UqYCr/
+  $(window).on('resize', function () { // grabbed from http://jsfiddle.net/UqYCr/
     if (document.body.clientWidth < 800) {
-      // $('.header-logo').removeClass('desktop-logo');
-      // $('.header-logo').addClass('mobile-logo');
-      // $('.menu-menu-1-container').hide()
-      // $('.menu-menu-1-container').css('display', 'none')
-      // $('#menu-item-37').addClass('science-logo')
-
     } else {
-      // $('.header-logo').removeClass('mobile-logo');
-      // $('.header-logo').addClass('desktop-logo');
-      // $('.menu-menu-1-container').show();
+      console.log('Desktop');
       $('.menu-menu-1-container').css('display', 'flex');
       $('.menu-menu-1-container').removeClass('menu-expanded');
       $('#menu-toggle').removeClass('is-active');
-      
     }
+
   })
-
-  // if (document.body.clientWidth < 800) {
-  //   $('#menu-item-37').before("<div class='science-logo'>yo</div>");
-  //   $('#menu-item-45').before("<div class='services-logo'>yo</div>");
-  //   $('#menu-item-38').before("<div class='praise-logo'>yo</div>");
-  //   $('#menu-item-39').before("<div class='contact-logo'>yo</div>");
-  // } else {
-  //   // $('#menu-item-37').remove("<p>yo</p>");
-  // }
-
 
   $('#menu-toggle').on('click', function () {
     $(this).toggleClass('is-active');
     $('.menu-menu-1-container').toggleClass('menu-expanded');
   })
+  
+  $('#menu-item-37').prepend('<div class="science-logo"></div>');
+  $('#menu-item-45').prepend('<div class="services-logo"></div>');
+  $('#menu-item-38').prepend('<div class="praise-logo"></div>');
+  $('#menu-item-39').prepend('<div class="contact-logo"></div>');
+
 })(jQuery);
