346871eaa916a81f9251c91ff8cfc102cbe09771
Cliff Chan <cchan17@live.ca>
2018-03-15 09:08:46 -0700

    changes to template php to match pages.php

--- themes/integrate-play/page-the-science-of-play.php
+++ themes/integrate-play/page-the-science-of-play.php
@@ -23,30 +23,3 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
-
-<?php
-/**
- * Template part for displaying page content in page.php.
- *
- * @package Integrate_Play_Theme
- */
-
-?>
-
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-</article><!-- #post-## -->
-<?php get_footer(); ?>
