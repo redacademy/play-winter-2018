0f32934d41bb4974ec74f9c825905bcf2a5eef25
Cliff Chan <cchan17@live.ca>
2018-03-15 09:26:58 -0700

    changes to templates

--- themes/integrate-play/page-praise.php
+++ themes/integrate-play/page-praise.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying the Praise page.
+ * The template for displaying all pages.
  *
  * @package Integrate_Play_Theme
  */
@@ -10,43 +10,16 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+			
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
-
-
 			<?php endwhile; // End of the loop. ?>
-
-
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-
-<?php
-/**
- * Template part for displaying page content in page.php.
- *
- * @package Integrate_Play_Theme
- */
-
-?>
-
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-</article><!-- #post-## -->
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
