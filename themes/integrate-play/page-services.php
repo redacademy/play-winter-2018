a845ca586d0fc5d12f9fec14b9c4f2fe7f6ba7eb
Eric Chan <ericchan277@gmail.com>
2018-03-15 14:58:17 -0700

    Added template file

--- themes/integrate-play/page-services.php
+++ themes/integrate-play/page-services.php
@@ -1,3 +1,21 @@
 <?php
-/* Page Template: Services Page */
-?>
+/* Template Name: Services Page */
+get_header(); ?>
+
+<div id="primary" class="content-area">
+  <main id="main" class="site-main" role="main">
+
+    
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+      <?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+    <?php endwhile; // End of the loop. ?>
+
+		<?php echo CFS()->get('contact_form');?>
+  </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
