ed8b5b3c34295288ed4524703626bfddc845bcaf
Rodrigo Samayoa <jorosafi@gmail.com>
2018-03-15 22:20:39 -0700

    adding styling to mobile version of contact us page and commenting buttons.js

--- themes/integrate-play/js/buttons.js
+++ themes/integrate-play/js/buttons.js
@@ -2,15 +2,17 @@
 
 (function ($) {
 
+  //function to make modal contact form on mobile to appear when users click buttons
   $('.mobile-btn').on('click',function(){
     $('#masthead').hide();
     $('#colophon').hide();
-    $('#main').hide();
+    $('#main').hide(); //make sure button and form are outside the #main content area
     $('#primary').css( "padding", "0" );
     $('.mobile-btn').hide();
     $('.mobile-contact-form').show();
   });
 
+  //This closes the modal window
   $('#close-btn').on('click',function(){
     $('#masthead').show();
     $('#colophon').show();
--- themes/integrate-play/page-contact-us.php
+++ themes/integrate-play/page-contact-us.php
@@ -10,16 +10,27 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			
-
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+				<div class="entry-content">
+					<?php the_content(); ?>
+				
+				</div><!-- .entry-content -->
+			</article><!-- #post-## -->
 
 			<?php endwhile; // End of the loop. ?>
-
+			<div class="social-icons">
+				<a href="https://twitter.com/KirstenatPlay" target="_blank" class="icon-frame"><i class="fa fa-twitter"></i></a>
+				<a href="https://www.instagram.com/explore/locations/160361740973203/integrate-play-solutions/" target="_blank" class="icon-frame"><i class="fa fa-instagram"></i></a>
+				<a href="https://www.facebook.com/IntegratePlaySolutions/" target="_blank" class="icon-frame"><i class="fa fa-facebook-f"></i></a>
+				<a href="https://www.linkedin.com/in/kirsten-anderson-910a7314/" target="_blank" class="icon-frame"><i class="fa fa-linkedin"></i></a>
+			</div>
 		</main><!-- #main -->
+
 		<?php echo CFS()->get('contact_form');?>
+
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
