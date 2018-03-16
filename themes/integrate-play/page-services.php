d6d10c987a2b7c6d292342750666fa686eae92e5
Eric Chan <ericchan277@gmail.com>
2018-03-15 19:20:36 -0700

    Added CFS calls in page-services.php

--- themes/integrate-play/page-services.php
+++ themes/integrate-play/page-services.php
@@ -5,15 +5,34 @@ get_header(); ?>
 <div id="primary" class="content-area">
   <main id="main" class="site-main" role="main">
 
-    
-
     <?php while ( have_posts() ) : the_post(); ?>
 
       <?php get_template_part( 'template-parts/content', 'page' ); ?>
 
     <?php endwhile; // End of the loop. ?>
 
-		<?php echo CFS()->get('contact_form');?>
+
+    <?php echo CFS()->get('services_workshops');?>
+    <?php echo CFS()->get('workshop_banner');?>
+    <?php echo CFS()->get('workshop_serious_play');?>
+    <?php echo CFS()->get('workshop_ways_to_play');?>
+
+    <?php echo CFS()->get('services_speaking');?>
+    <?php echo CFS()->get('speaking_banner');?>
+    <?php echo CFS()->get('speaking_international_speaker');?>
+    <?php echo CFS()->get('speaking_highlights');?>
+    <?php echo CFS()->get('speaking_keynote_topics');?>
+    
+    <?php echo CFS()->get('services_coaching');?>
+    <?php echo CFS()->get('coaching_banner');?>
+    <?php echo CFS()->get('coaching_here_for_you');?>
+    <?php echo CFS()->get('coaching_advice');?>
+
+    <?php echo CFS()->get('contact_form');?>
+ 
+
+
+
   </main><!-- #main -->
 </div><!-- #primary -->
 
