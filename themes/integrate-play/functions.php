3b0e97df7e118369272106fa3235cc4edcc37a4c
Eric Chan <ericchan277@gmail.com>
2018-03-16 22:56:18 -0700

    Modified font enqueueing, deleted empty scss file, removed fontimport from style.scss, created services.scss

--- themes/integrate-play/functions.php
+++ themes/integrate-play/functions.php
@@ -94,9 +94,7 @@ function integrate_play_scripts() {
 
 	wp_enqueue_style('font-awesome-cdn','https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', array(),
 	'4.7.0'); 
-	wp_enqueue_style('muli-font-cdn','https://fonts.googleapis.com/css?family=Muli:300,400,600,700');
-	wp_enqueue_style('oswald-font-cdn','https://fonts.googleapis.com/css?family=Oswald:700');
-
+	wp_enqueue_style('google-font-cdn','https://fonts.googleapis.com/css?family=Muli:300i,400,600,700|Oswald:700');
 
 	wp_enqueue_script( 'integrate-scripts', get_template_directory_uri() . '/build/js/integrate-scripts.min.js', array(), '23', true );
 	wp_enqueue_script( 'buttons', get_template_directory_uri() . '/build/js/buttons.min.js', array(), '23', true );
