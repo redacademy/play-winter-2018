f95d0efdb2e4e92dc6a0c6c966ee61f70e01acb7
Eric Chan <ericchan277@gmail.com>
2018-03-16 22:58:21 -0700

    Added jquery as a dependency for script files

--- themes/integrate-play/functions.php
+++ themes/integrate-play/functions.php
@@ -96,8 +96,8 @@ function integrate_play_scripts() {
 	'4.7.0'); 
 	wp_enqueue_style('google-font-cdn','https://fonts.googleapis.com/css?family=Muli:300i,400,600,700|Oswald:700');
 
-	wp_enqueue_script( 'integrate-scripts', get_template_directory_uri() . '/build/js/integrate-scripts.min.js', array(), '23', true );
-	wp_enqueue_script( 'buttons', get_template_directory_uri() . '/build/js/buttons.min.js', array(), '23', true );
+	wp_enqueue_script( 'integrate-scripts', get_template_directory_uri() . '/build/js/integrate-scripts.min.js', array(jquery), '23', true );
+	wp_enqueue_script( 'buttons', get_template_directory_uri() . '/build/js/buttons.min.js', array(jquery), '23', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
