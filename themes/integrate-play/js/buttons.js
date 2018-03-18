2ecc16d16724293747f3b3173363e1e3876fd64b
Rodrigo Samayoa <jorosafi@gmail.com>
2018-03-17 20:35:52 -0700

    cleaning quotes in buttons.js and fixing hover state on contact buttons

--- themes/integrate-play/js/buttons.js
+++ themes/integrate-play/js/buttons.js
@@ -2,41 +2,40 @@
 
 (function ($) {
   $( document ).ready(function() {
-//function to make modal contact form on mobile to appear when users click buttons
+  //function to make modal contact form on mobile to appear when users click buttons
     $('.mobile-btn').on('click',function(){
-      // $('body').append('<div class="lbox">L Box</div>');
       $('#masthead').hide();
       $('#colophon').hide();
       $('.testimonials').hide();
       $('#main').hide(); //make sure button and form are outside the #main content area
-      $('#primary').css( "padding", "0" );
+      $('#primary').css( 'padding', '0' );
       $('.mobile-btn').hide();
       $('.mobile-contact-form').show();
     });
 
-//This closes the modal window
+  //This closes the modal window
     $('#close-btn').on('click',function(){
       $('#masthead').show();
       $('#colophon').show();
       $('.testimonials').show();
       $('#main').show();
-      $('#primary').css( "padding", "" );
+      $('#primary').css( 'padding', '' );
       $('.mobile-btn').show();
       $('.mobile-contact-form').hide();
     });
   });
 
-//This shows full quote on the praise page when user clicks on See More
-  $('.read-more').on('click', function(e){
-    e.preventDefault();
-    // console.log('button works');
-    $(this).parent().parent().hide();
-    $(this).parent().parent().prev().show();
-  })
+  //This shows full quote on the praise page when user clicks on See More
+    $('.read-more').on('click', function(e){
+      e.preventDefault();
+      // console.log('button works');
+      $(this).parent().parent().hide();
+      $(this).parent().parent().prev().show();
+    })
   
-  $('.read-less').on('click', function(e){
-    e.preventDefault();
-       $(this).parent().hide();
-       $(this).parent().next().show();
-  })
+    $('.read-less').on('click', function(e){
+      e.preventDefault();
+        $(this).parent().hide();
+        $(this).parent().next().show();
+    })
 })(jQuery);
