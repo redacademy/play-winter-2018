72d198411822bb6d36222936beee47fbab12a3c1
Rodrigo Samayoa <jorosafi@gmail.com>
2018-03-17 20:24:06 -0700

    adding featured image to default page

--- themes/integrate-play/template-parts/content-page.php
+++ themes/integrate-play/template-parts/content-page.php
@@ -9,7 +9,10 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	<?php if ( has_post_thumbnail() ) : ?>
+			<?php the_post_thumbnail( 'large' ); ?>
+		<?php endif; ?>	
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
