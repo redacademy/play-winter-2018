d22df48fdb4d2ef339db540eadfd937cbe5004dd
Rodrigo Samayoa <jorosafi@gmail.com>
2018-03-20 11:01:00 -0700

    add full style for 404 page

--- themes/integrate-play/404.php
+++ themes/integrate-play/404.php
@@ -19,12 +19,14 @@ get_header(); ?>
 
 				<div class="page-content">
 					<p><?php echo esc_html( 'It looks like nothing was found at this location. Maybe try one of the links below?' ); ?></p>
-			
-				<a href="http://play.van.cp.academy.red/the-science-of-play/">The Science of Play</a>
-				<a href="http://play.van.cp.academy.red/services/#workshop">Workshops</a>
-				<a href="http://play.van.cp.academy.red/services/#speaking">Professional Speaking</a>
-				<a href="http://play.van.cp.academy.red/services/#coaching">Coaching</a>
-				<a href="http://play.van.cp.academy.red/contact-us/">Contact Us</a>
+				<div class="links">
+					<a href="http://play.van.cp.academy.red/the-science-of-play/">The Science of Play</a>
+					<a href="http://play.van.cp.academy.red/services/#workshop">Workshops</a>
+					<a href="http://play.van.cp.academy.red/services/#speaking">Professional Speaking</a>
+					<a href="http://play.van.cp.academy.red/services/#coaching">Coaching</a>
+					<a href="http://play.van.cp.academy.red/contact-us/">Contact Us</a>
+				</div>
+				
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
