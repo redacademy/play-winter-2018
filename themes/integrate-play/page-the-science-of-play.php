7f8a8d88cb43984ca7e90c9b2b67a70ba0221f9e
Rodrigo Samayoa <jorosafi@gmail.com>
2018-03-19 18:07:51 -0700

    Update the science page banner to make it a CSS background instead

--- themes/integrate-play/page-the-science-of-play.php
+++ themes/integrate-play/page-the-science-of-play.php
@@ -12,10 +12,15 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<?php if ( has_post_thumbnail() ) : ?>
-							<?php the_post_thumbnail( 'large' ); ?>
+
+			<?php if ( has_post_thumbnail() ) : ?>
+							<?php $p_img = get_the_post_thumbnail_url(  ); 
+							?>
 					<?php endif; ?>	
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>
+				style="background:url('<?php echo $p_img; ?>'); width: 100%; height: 300px;">
+				
 					<header class="entry-header">
 						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 					
@@ -47,6 +52,11 @@ get_header(); ?>
 				<div class="science-cfs-container"><?php echo CFS()->get('science_content_block_3');?>	</div>
 			</div>
 
+			<div class="bubbles small"></div>
+			<div class="bubbles medium"></div>
+			<div class="bubbles large"></div>
+			<div class="bubbles xl"></div>
+			<div class="bubbles xxl"></div>
 		</main><!-- #main -->
 
 		<div class="contact-form-wrapper"><?php echo CFS()->get('contact_form');?>	</div>
